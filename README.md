80f4283ee04b969ab525983b53d9cb2a3a767a3e
gznyyb <xl2723@columbia.edu>
2019-04-30 19:50:17 -0400

    added requirement.txt and edit the readme file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# painting-classification
+Painting Classification
+-------------------------
+
+Investigate how data availability affects the performance of the model, predict the identity of the artist given paintings, and evaluate the model performance by visualizing saliency maps. 
+
+Installation
+-------------------------
+
+### Install the requirements
+* To run the notebook, please install the required packages using `pip install -r requirements.txt`. 
+* Please make sure to use Python 3 for this notebook.
+
+Run the code
+--------------------------
+* Run `python load_imgs.py` to start the preprocessing of images and split the data into train, validation, and test set. Please make sure to have enough disk space to hold these files. 
+* run the notebook. 
--- load_imgs.py
+++ load_imgs.py
@@ -6,49 +6,54 @@ Created on Sun Mar 31 09:40:33 2019
 @author: xianghongluo
 """
 
+# import packages for processing images
 from PIL import Image
+from tensorflow.keras.preprocessing import image as kp_image
+
+# import other useful packages
 import os
 import numpy as np
 import glob
 import pickle
-
-
-from tensorflow.keras.preprocessing import image as kp_image
 from sklearn.model_selection import train_test_split
 
-import pickle
-
-
+# path to all the images
 image_dir_path = '../best-artworks-of-all-time/resized'
 
+# array to store the images and their respective labels 
 image_data = []
 labels = []
 
+# loop to process all images and store the images and labels
 for filename in glob.glob(os.path.join(image_dir_path,'*.jpg')):
+    # get the label
     img_str_list = filename.split('\\')[-1].split('_')
     if len(img_str_list) == 2:
         labels.append(img_str_list[0])
     else:
         labels.append(' '.join(img_str_list[:2]))
-    img = Image.open(filename)
-    img = img.resize((331, 331), Image.ANTIALIAS)
-    img = kp_image.img_to_array(img)
+
+    # process the image
+    img = Image.open(filename) # open the image file
+    img = img.resize((331, 331), Image.ANTIALIAS) # resize the image
+    img = kp_image.img_to_array(img) # convert it to array 
+    # copy the black and white images across color channel such that their dimensions
+    # match the colored images'
     if img.shape[-1] != 3:
         img = np.repeat(img, 3, axis=2)
     img = np.expand_dims(img, axis=0)
-    image_data.append(img)
+    image_data.append(img) # store the image arrays 
 
-
+# convert the list of image arrays into a big array
 image_data = np.concatenate(image_data, axis=0)
 
-
+# convert the string labels into numeric labels
 unique_labels = sorted(set(labels))
 name2idx = {label: index for index, label in enumerate(unique_labels)}
 idx2name = np.array(unique_labels)
-
 int_labels = [name2idx[named_label] for named_label in labels]
 
-
+# split the data into training, validation, and test sets
 X_train, X_test, y_train, y_test = train_test_split(image_data, int_labels,
                                                     test_size=0.25,
                                                     random_state=10,
@@ -59,6 +64,7 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
                                                   random_state=10,
                                                   stratify=y_train)
 
+# save the training, validation, and test sets to disk 
 np.save('../X_train.npy', arr=X_train)
 np.save('../X_val.npy', arr=X_val)
 np.save('../X_test.npy', arr=X_test)
@@ -66,6 +72,7 @@ np.save('../y_train.npy', arr=y_train)
 np.save('../y_val.npy', arr=y_val)
 np.save('../y_test.npy', arr=y_test)
 
+# save the labels to disk
 with open('../name2idx.pickle', 'wb') as handle:
     pickle.dump(name2idx, handle)
 
